b79ef0bfaa264d1af76d16007cdbc7cb40c0b4b2
mundanecoder <dipankar.laskar45@gmail.com>
2024-08-19 13:59:54 +0530

    changes to mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Exclude modules that are only meant to be used on the server side
+      config.externals.push("bcrypt", "fs");
+
+      // Handle any potential issues with @mapbox/node-pre-gyp
+      config.module.rules.push({
+        test: /\.html$/,
+        use: "null-loader", // Ignore HTML files to prevent bundling issues
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
