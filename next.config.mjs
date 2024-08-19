83f359687948f07676d1c6cfb299bfc93c1d6328
mundanecoder <dipankar.laskar45@gmail.com>
2024-08-19 14:01:30 +0530

    changes tomjs with rm fs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // Exclude modules that are only meant to be used on the server side
-      config.externals.push("bcrypt", "fs");
+      config.externals.push("bcrypt");
 
       // Handle any potential issues with @mapbox/node-pre-gyp
       config.module.rules.push({
